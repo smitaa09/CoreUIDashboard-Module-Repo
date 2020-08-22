6a5859ab4c350d4c1506958b69532b11ab496103
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2020-08-22 22:50:28 +0530

    Adding file

--- s3/main.tf
+++ s3/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   version = "~> 2.0"
-  region  = "ap-east-1"
+  region  = "us-east-1"
 }
 resource "aws_s3_bucket" "s3Bucket" {
-  bucket = "react-aws-s3bucket1598116299728"
-  acl    = "private-read"
+  bucket = "react-aws-s3bucket1598116805162"
+  acl    = "private"
 }
 
 

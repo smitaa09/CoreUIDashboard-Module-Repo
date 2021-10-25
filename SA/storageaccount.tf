cd8d5931f5e4a2ffe1daed51e07ae9b99f0150a2
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2021-10-25 09:48:53 +0000

    Update storageaccount.tf

--- SA/storageaccount.tf
+++ SA/storageaccount.tf
@@ -16,7 +16,7 @@ provider "azurerm" {
 
 resource "azurerm_storage_account" "sa" {
   name                     = "testsa001"
-  resource_group_name      = "ANI_Digital_Lab"
+  resource_group_name      = "rg-ani-c-001"
   location                 = "eastus"
   account_tier             = "Standard"
   account_replication_type = "LRS"

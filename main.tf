8a4c904f75bebe3fcab1a9226f92207c0662c817
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2021-10-25 10:03:01 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ provider "azurerm" {
 
 resource "azurerm_storage_account" "sa" {
   name                     = "testsa01"
-  resource_group_name      = "ANI_Digital_Lab"
+  resource_group_name      = "rg-ani-c-001"
   location                 = "eastus"
   account_tier             = "Standard"
   account_replication_type = "GRS"

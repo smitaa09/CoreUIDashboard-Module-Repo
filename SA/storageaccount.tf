6819a035fa5a3ecb9338ddd34ff9ca924d31c4b3
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2021-10-25 09:25:14 +0000

    updated sa

--- SA/storageaccount.tf
+++ SA/storageaccount.tf
@@ -1,9 +1,21 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.61.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = "testsa001"
+  name                     = "testsa01"
   resource_group_name      = "ANI_Digital_Lab"
   location                 = "eastus"
   account_tier             = "Standard"

ae2058ef27116c69f8208fdc96cb81c2e9be1780
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2021-10-25 09:40:36 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,23 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "2.61.0"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_storage_account" "sa" {
+  name                     = "testsa01"
+  resource_group_name      = "ANI_Digital_Lab"
+  location                 = "eastus"
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}

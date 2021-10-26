f9258692de2168347234d67c5d1d858e6a912e41
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2021-10-26 11:01:53 +0000

    Adding file

--- EC2/EC2.tf
+++ EC2/EC2.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "from-terraform" {
-  ami           = "ami-08f3d892de259504d"
+  ami           = "ami-0416f96ae3d1a3f29"
   instance_type = "t2.micro"
 
 tags = {
-    Name = "aws-EC2-Instance"
-    Env = "dev"
+    Name = "TestInstance"
+    Env = "poc"
   }
 }

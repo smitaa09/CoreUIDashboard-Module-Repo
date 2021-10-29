4b20dda57ba5755309afb6a95452c3f83d962dd6
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2021-10-29 06:18:55 +0000

    Adding file

--- Azure/VM/VM.tf
+++ Azure/VM/VM.tf
@@ -21,12 +21,12 @@ data "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "TestVM01-nic"
+  name                = "TestVM01"+"-nic"
   location            = "eastus"
   resource_group_name = "rg-ani-c-001"
 
   ip_configuration {
-    name                          = "TestVM01-config"
+    name                          = "TestVM01"+"-config"
     subnet_id                     = "${data.azurerm_subnet.subnet.id}"
     private_ip_address_allocation = "Dynamic"
   }
@@ -34,8 +34,8 @@ resource "azurerm_network_interface" "nic" {
 
 resource "azurerm_virtual_machine" "virtualmachine" {  
   name                  = "TestVM01"
-  location              = azurerm_network_interface.nic.location
-  resource_group_name   = azurerm_network_interface.nic.resource_group_name
+  location              = "eastus"
+  resource_group_name   = "rg-ani-c-001"
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_B1s"
 
@@ -46,7 +46,7 @@ resource "azurerm_virtual_machine" "virtualmachine" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "TestVM01-osDisk01"
+    name              = "TestVM01"+"-osDisk01"
     os_type           = "Windows"
     caching           = "ReadWrite"
     create_option     = "FromImage"
@@ -54,9 +54,9 @@ resource "azurerm_virtual_machine" "virtualmachine" {
     disk_size_gb      = "127" 
   }
   os_profile {
-    computer_name  = "TestVM001"
+    computer_name  = "TestVM01"
     admin_username = "cloudadmin"
-    admin_password = "password@123"
+    admin_password = "Password@123"
   }
     os_profile_windows_config { 
   }

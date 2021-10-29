5921a01a34b6c0a4e98bb1b46bbd1d5081c1835a
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2021-10-29 06:48:15 +0000

    Adding file

--- Azure/VM/VM.tf
+++ Azure/VM/VM.tf
@@ -21,43 +21,47 @@ data "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "TestVM01-nic"
+  name                = "TestVM02-nic"
   location            = "eastus"
   resource_group_name = "rg-ani-c-001"
 
   ip_configuration {
-    name                          = "TestVM01-config"
+    name                          = "TestVM02-config"
     subnet_id                     = "${data.azurerm_subnet.subnet.id}"
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "virtualmachine" {  
-  name                  = "TestVM01"
+  name                  = "TestVM02"
   location              = "eastus"
   resource_group_name   = "rg-ani-c-001"
   network_interface_ids = [azurerm_network_interface.nic.id]
   vm_size               = "Standard_B1s"
 
   storage_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2019-Datacenter"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
   storage_os_disk {
-    name              = "TestVM01-osDisk01"
-    os_type           = "Windows"
+    name              = "TestVM02-osDisk01"
+    os_type           = "Linux"
     caching           = "ReadWrite"
     create_option     = "FromImage"
     managed_disk_type = "Standard_LRS"
     disk_size_gb      = "127" 
   }
   os_profile {
-    computer_name  = "TestVM01"
+    computer_name  = "TestVM02"
     admin_username = "cloudadmin"
     admin_password = "Password@123"
   }
     os_profile_windows_config { 
+  }
+
+  os_profile_linux_config {
+    
   }
 }

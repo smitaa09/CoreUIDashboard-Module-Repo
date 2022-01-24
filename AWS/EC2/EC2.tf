3dacadeb3ae10ee74c55d4c1167303e287d32d52
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2022-01-24 14:09:26 +0530

    Adding file

--- AWS/EC2/EC2.tf
+++ AWS/EC2/EC2.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "from-terraform" {
-  ami           = "ami-0892d3c7ee96c0bf7"
-  instance_type = "t2.nano"
+  ami           = "ami-0f18e475ccdc50e07"
+  instance_type = "t2.micro"
 
 tags = {
-    Name = "testVM"
-    Env = "POC"
+    Name = "testVM01"
+    Env = "poc"
   }
 }

3051f1f5947c489d47353b4b16ac827dd9b18efd
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2022-02-04 08:14:35 +0000

    Update EC2.tf

--- AWS/EC2/EC2.tf
+++ AWS/EC2/EC2.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_instance" "from-terraform" {
-  ami           = "ami-07dc734dc14746eab"
+  ami           = "ami-0aad84f764a2bd39a"
   instance_type = "t2.micro"
 
 tags = {

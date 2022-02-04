054cfa1c06fc02306ed2b0d775958925aa8de336
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2022-02-04 11:46:47 +0530

    Adding file

--- AWS/S3/S3.tf
+++ AWS/S3/S3.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   version = "~> 2.0"
-  region  = "ap-east-1"
+  region  = "us-east-1"
 }
 resource "aws_s3_bucket" "s3Bucket" {
-  bucket = "demobucket1643118795049"
+  bucket = "demobucket1643955331059"
   acl    = "private"
 }

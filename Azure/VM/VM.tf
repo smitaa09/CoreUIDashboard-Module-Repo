2c4f264fc1a8e5683b318a8117f107e72b7b38bc
smitaa09 <57519934+smitaa09@users.noreply.github.com>
2022-02-10 13:06:58 +0000

    Adding file

--- Azure/VM/VM.tf
+++ Azure/VM/VM.tf
@@ -21,19 +21,19 @@ data "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_interface" "nic" {
-  name                = "TestVM01-nic"
+  name                = "DemoVM01-nic"
   location            = "eastus"
   resource_group_name = "rg-ani-c-001"
 
   ip_configuration {
-    name                          = "TestVM01-config"
+    name                          = "DemoVM01-config"
     subnet_id                     = "${data.azurerm_subnet.subnet.id}"
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "virtualmachine" {  
-  name                  = "TestVM01"
+  name                  = "DemoVM01"
   location              = "eastus"
   resource_group_name   = "rg-ani-c-001"
   network_interface_ids = [azurerm_network_interface.nic.id]
@@ -46,7 +46,7 @@ resource "azurerm_virtual_machine" "virtualmachine" {
     version   = "latest"
   }
   storage_os_disk {
-    name              = "TestVM01-osDisk01"
+    name              = "DemoVM01-osDisk01"
     os_type           = "Windows"
     caching           = "ReadWrite"
     create_option     = "FromImage"
@@ -54,7 +54,7 @@ resource "azurerm_virtual_machine" "virtualmachine" {
     disk_size_gb      = "128" 
   }
   os_profile {
-    computer_name  = "TestVM01"
+    computer_name  = "DemoVM01"
     admin_username = "cloudadmin"
     admin_password = "TCS@12345"
   }
